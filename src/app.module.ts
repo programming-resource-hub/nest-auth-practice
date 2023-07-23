48586d142bc59abb71559286f3324725ea11b0f3
devcraft1 <ukonudennis64@gmail.com>
2023-07-22 15:02:17 -1100

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [CoffeesModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
