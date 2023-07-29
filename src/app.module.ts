efb2b4dc40c9552cce5adf6c0a4ebb49c929cef3
devcraft1 <ukonudennis64@gmail.com>
2023-07-29 11:54:48 +0100

    --

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesModule } from './coffees/coffees.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CoffeesModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
